232258a5602f3c24c47c56f03e48375361e25971
Pooja B H <pooja.hundekar5@gmail.com>
2019-11-24 14:27:02 +0530

    bootstrap added  to the application

--- employeeregistration/src/app/employee/employee.component.ts
+++ employeeregistration/src/app/employee/employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Employee } from './entity/employee';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
@@ -9,7 +9,21 @@ export class EmployeeComponent implements OnInit {
 
   constructor() { }
 
+ employeeList : Employee[] = [];
   ngOnInit() {
+    console.log('calling');
+this.getEmployees();
   }
 
+  getEmployees(){
+    this.employeeList =list;
+    console.log(this.employeeList);
+  }
 }
+
+export const list : Employee[]=[{
+  firstName: 'Pooja',
+  lastName: 'H',
+  gender: 'FEMALE',
+  department:''
+}];
---
+++ employeeregistration/src/app/employee/entity/employee.ts
@@ -0,0 +1,8 @@
+export class Employee {
+    firstName: string;
+    lastName: string;
+    gender: string;
+    department: string;
+    
+}
+
