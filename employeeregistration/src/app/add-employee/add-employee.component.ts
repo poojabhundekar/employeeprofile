ce3d02f2804941b0114fb347b32ce8fe42f19a0c
Pooja B H <pooja.hundekar5@gmail.com>
2019-11-24 23:05:55 +0530

    fontaawesome added

--- employeeregistration/src/app/add-employee/add-employee.component.ts
+++ employeeregistration/src/app/add-employee/add-employee.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators  } from '@angular/forms';
 
 @Component({
   selector: 'app-add-employee',
@@ -10,14 +10,20 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class AddEmployeeComponent implements OnInit {
   employeeForm: FormGroup;
   departmentList:any = ["Software","Harddware"];
-  constructor(private router : Router ) { }
+  constructor(private router : Router, private formBuilder: FormBuilder ) { }
   ngOnInit() {
-    this.employeeForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      gender: new FormControl()
-      // department:[""]
-    });
+    // this.employeeForm = new FormGroup({
+    //   firstName: new FormControl(),
+    //   lastName: new FormControl(),
+    //   gender: new FormControl()
+    //   // department:[""]
+    // });
+    this.employeeForm = this.formBuilder.group({
+      firstName: ['',  Validators.required],
+      lastName: ['',  Validators.required],
+      department: ['',  Validators.required], 
+    }
+    );
   }
   gobackToViewScreen(){
         this.router.navigate(['employees']);
