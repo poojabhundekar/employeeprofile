dfe5e944ec7d2f01dac856810452a10f04a1c348
Pooja B H <pooja.hundekar5@gmail.com>
2019-11-24 12:31:32 +0530

    employee component added

--- employeeregistration/src/app/app-routing.module.ts
+++ employeeregistration/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'employees',
+    component: EmployeeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
